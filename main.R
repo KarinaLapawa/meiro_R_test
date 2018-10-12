0c2606d9cc43fcfe52c355fa063e0d98e5455079
KarinaLapawa <33736751+KarinaLapawa@users.noreply.github.com>
2018-10-12 15:34:42 +0200

    Update main.R

--- main.R
+++ main.R
@@ -1,30 +1,9 @@
-# meiro_R_test
-library(dplyr)
-#for computing mean and piping operator usage
+#import necessary libraries
+library(data.table)
 
-# titanic:
-# Survived
-# Pclass
-# Name
-# Sex
-# Age
-# Siblongs.Spouses.Aboad
-# Parents.Children.Aboard
-# Fare
+#request url and save response to variable titanic
+titanic<-fread("http://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv")
 
-#read file
-titanic_in<-read.csv("/data/in/files/titanic.csv",stringsAsFactors = FALSE)
-
-#compute mean of ages and fares of all passengers and store it in data frame
-mean_all<-data.frame(titanic_in%>%summarise(mean_age=mean(Age),mean_fare=mean(Fare)))
-
-#filter table for only male passengers who survived
-data_selected_male<-subset(titanic_in,Sex=='male'& Survived==1)
-
-#compute mean of ages and fares of those male and survived and store it in data frame
-mean_male_survived<-data.frame(data_selected_male%>%summarise(mean_age=mean(Age),mean_fare=mean(Fare)))
-
-# write output means to tables
-write.csv(mean_all, file = "/data/out/files/mean_all.csv", row.names = FALSE)
-write.csv(mean_male_survived, file = "/data/out/files/mean_male_survived.csv", row.names = FALSE)
-
+# write output to table and print first 10 rows
+write.csv(titanic, file = "/data/out/tables/titanic.csv", row.names = FALSE)
+print(titanic[1:10,"Survived"])
