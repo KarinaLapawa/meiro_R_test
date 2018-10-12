fd9a0ba5fcf0a652d835a9649cba3629d335154b
KarinaLapawa <33736751+KarinaLapawa@users.noreply.github.com>
2018-10-12 13:18:24 +0200

    Update main.R

--- main.R
+++ main.R
@@ -13,7 +13,7 @@ library(dplyr)
 # Fare
 
 #read file
-titanic_in<-read.csv("in/files/titanic.csv",stringsAsFactors = FALSE)
+titanic_in<-read.csv("data/in/files/titanic.csv",stringsAsFactors = FALSE)
 
 #compute mean of ages and fares of all passengers and store it in data frame
 mean_all<-data.frame(titanic_in%>%summarise(mean_age=mean(Age),mean_fare=mean(Fare)))
@@ -25,6 +25,6 @@ data_selected_male<-subset(titanic_in,Sex=='male'& Survived==1)
 mean_male_survived<-data.frame(data_selected_male%>%summarise(mean_age=mean(Age),mean_fare=mean(Fare)))
 
 # write output means to tables
-write.csv(mean_all, file = "out/files/mean_all.csv", row.names = FALSE)
-write.csv(mean_male_survived, file = "out/files/mean_male_survived.csv", row.names = FALSE)
+write.csv(mean_all, file = "data/out/files/mean_all.csv", row.names = FALSE)
+write.csv(mean_male_survived, file = "data/out/files/mean_male_survived.csv", row.names = FALSE)
 
