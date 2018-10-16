08a8ec202ef8aa174260cccdf8957e3786309c38
KarinaLapawa <33736751+KarinaLapawa@users.noreply.github.com>
2018-10-16 14:32:36 +0200

    Update main.R

--- main.R
+++ main.R
@@ -1,4 +1,16 @@
-install.packages("RJSONIO")
-library(RJSONIO) 
-file <- RJSONIO::fromJSON("/data/config.json")
-file [['parameters']][['Username']]
+
+install.packages(“RJSONIO”)
+library(RJSONIO)
+file<-RJSONIO::fromJSON("/data/config.json")
+file[[‘parameters’]][[‘url’]]
+
+
+#import necessary libraries
+library(data.table)
+
+#request url and save response to variable titanic
+titanic<-fread("url")
+
+# write output to table and print first 10 rows
+write.csv(titanic, file = "/data/out/tables/titanic.csv", row.names = FALSE)
+print(titanic[1:10,"Survived"])
