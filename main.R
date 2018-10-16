e0278af0f0932fb03a280d13e00508d0eabe3a23
KarinaLapawa <33736751+KarinaLapawa@users.noreply.github.com>
2018-10-16 16:32:24 +0200

    Update main.R

--- main.R
+++ main.R
@@ -4,14 +4,14 @@
 #request url and save response to variable titanic
 install.packages("RJSONIO")
 library(RJSONIO)
-MeiroP<-RJSONIO::fromJSON("/data/config.json")
-MeiroP[['parameters']][['url']]
+Meiroparameter<-RJSONIO::fromJSON("/data/config.json")
+Meiroparameter[['parameters']][['url']]
 
 #import necessary libraries
 library(data.table)
 
 #request url and save response to variable titanic
-titanic<-fread ( "MeiroP" )
+titanic<-fread ( "Meiroparameter" )
 
 # write output to table and print first 10 rows
 write.csv(titanic, file = "/data/out/tables/titanic.csv", row.names = FALSE)
