53966a775b53536e22d9e8eae1efa79fbfab539a
KarinaLapawa <33736751+KarinaLapawa@users.noreply.github.com>
2018-10-16 16:15:50 +0200

    Update main.R

--- main.R
+++ main.R
@@ -11,7 +11,7 @@ file[['parameters']][['url']]
 library(data.table)
 
 #request url and save response to variable titanic
-titanic<-fread[['parameters']][['url']]
+titanic<-fread ([['parameters']][['url']])
 
 # write output to table and print first 10 rows
 write.csv(titanic, file = "/data/out/tables/titanic.csv", row.names = FALSE)
